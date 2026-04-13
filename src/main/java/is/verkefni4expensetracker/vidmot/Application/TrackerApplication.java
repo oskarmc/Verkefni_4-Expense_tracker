01e4c90d6a18eb2c549ac294b1a4a8f6030527f0
oskar <oskarmichal06@gmail.com>
2026-04-13 09:58:38 +0000

    Bætti við einfalt CSS útlit

--- src/main/java/is/verkefni4expensetracker/vidmot/Application/TrackerApplication.java
+++ src/main/java/is/verkefni4expensetracker/vidmot/Application/TrackerApplication.java
@@ -13,6 +13,9 @@ public class TrackerApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TrackerApplication.class.getResource("/is/verkefni4expensetracker/adal-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 400);
+        scene.getStylesheets().add(
+                TrackerApplication.class.getResource("/is/verkefni4expensetracker/main.css").toExternalForm()
+        );
         ViewSwitcher.setScene(scene);
         stage.setTitle("Expense tracker");
         stage.setScene(scene);
